425fb98bd656ec758eb5caa883d2eda6b1be034e
Nassimakhandari <nassomanassima37@gmail.com>
2024-08-14 18:36:17 +0100

    add carousel dans le section menu

--- public/js/app.js
+++ public/js/app.js
@@ -9,80 +9,108 @@ loginBtn.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-// ! carousel 1 -- watch video
-let nextBtns = document.querySelectorAll(".next");
-let prevBtns = document.querySelectorAll(".prev");
-let containers = document.querySelectorAll(".carousel");
+// // ! carousel 1 -- watch video
+// let nextBtns = document.querySelectorAll(".next");
+// let prevBtns = document.querySelectorAll(".prev");
+// let containers = document.querySelectorAll(".carousel");
+
+// let currentIndex = 0;
+
+// const slideImage = (index, myBtn) => {
+//     let carouselBtnAttribute = myBtn.getAttribute("carouselBtn");
+
+//     containers.forEach(container => {
+//         if (container.id === carouselBtnAttribute) {
+//             let slides = container.querySelectorAll(".slide");
+//             let camera = container.querySelector(".carousel");
+//             let slideWidth = slides[0].clientWidth;
+//             let indicators = container.querySelectorAll('.indicator');
+
+//             if (index < 0) {
+//                 index = slides.length - 1; 
+//             } else if (index >= slides.length) {
+//                 index = 0; 
+//             }
+
+//             indicators.forEach(indicator => {
+//                 indicator.classList.remove('activeIndicator');
+//             });
+//             indicators[index].classList.add("activeIndicator");
+
+//             camera.style.transform = `translateX(-${slideWidth * index}px)`;
+//             currentIndex = index;
+//         }
+//     });
+// };
+
+// nextBtns.forEach(next => {
+//     next.addEventListener("click", (event) => {
+//         let container = next.closest(".carousel");
+//         slideImage(currentIndex + 1, event.target);
+//     });
+// });
+
+// prevBtns.forEach(prev => {
+//     prev.addEventListener("click", (event) => {
+//         let container = prev.closest(".carousel");
+//         slideImage(currentIndex - 1, event.target);
+//     });
+// });
+
+// containers.forEach(container => {
+//     if (container.getAttribute("autoslide")) {
+//         let nextBtn = container.querySelector(".next");
+
+//         setInterval(() => {
+//             nextBtn.click(); 
+//         }, 3000); 
+//     }
+// });
+
+// containers.forEach(container => {
+//     let slides = container.querySelectorAll(".slide");
+//     let indicatorsGrp = document.createElement("div");
+//     indicatorsGrp.classList.add("indicators-grp");
+//     container.appendChild(indicatorsGrp);
+
+//     slides.forEach(() => {
+//         let indicator = document.createElement("div");
+//         indicator.classList.add("indicator");
+//         indicatorsGrp.appendChild(indicator);
+//     });
+
+//     indicatorsGrp.querySelector(".indicator").classList.add('activeIndicator');
+// });
+
+// document.getElementById("watchVideo").addEventListener("click", () => {
+//     let carousel = document.getElementById("carousel1");
+//     carousel.style.display = "flex";
+// });
+
+
+let carousel = document.querySelector('.carousel3');
+let slides = document.querySelectorAll('.slide');
 
 let currentIndex = 0;
+let total = slides.length;
+let slideInterval = 3000; 
 
-const slideImage = (index, myBtn) => {
-    let carouselBtnAttribute = myBtn.getAttribute("carouselBtn");
-
-    containers.forEach(container => {
-        if (container.id === carouselBtnAttribute) {
-            let slides = container.querySelectorAll(".slide");
-            let camera = container.querySelector(".carousel");
-            let slideWidth = slides[0].clientWidth;
-            let indicators = container.querySelectorAll('.indicator');
-
-            if (index < 0) {
-                index = slides.length - 1; 
-            } else if (index >= slides.length) {
-                index = 0; 
-            }
-
-            indicators.forEach(indicator => {
-                indicator.classList.remove('activeIndicator');
-            });
-            indicators[index].classList.add("activeIndicator");
-
-            camera.style.transform = `translateX(-${slideWidth * index}px)`;
-            currentIndex = index;
-        }
-    });
-};
-
-nextBtns.forEach(next => {
-    next.addEventListener("click", (event) => {
-        let container = next.closest(".carousel");
-        slideImage(currentIndex + 1, event.target);
-    });
-});
-
-prevBtns.forEach(prev => {
-    prev.addEventListener("click", (event) => {
-        let container = prev.closest(".carousel");
-        slideImage(currentIndex - 1, event.target);
-    });
-});
-
-containers.forEach(container => {
-    if (container.getAttribute("autoslide")) {
-        let nextBtn = container.querySelector(".next");
-
-        setInterval(() => {
-            nextBtn.click(); 
-        }, 3000); 
+function start(index) {
+    if (index >= total) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = total - 1;
+    } else {
+        currentIndex = index;
     }
-});
 
-containers.forEach(container => {
-    let slides = container.querySelectorAll(".slide");
-    let indicatorsGrp = document.createElement("div");
-    indicatorsGrp.classList.add("indicators-grp");
-    container.appendChild(indicatorsGrp);
+    carousel.style.transform = `translateX(-${100 * currentIndex}%)`;
+}
 
-    slides.forEach(() => {
-        let indicator = document.createElement("div");
-        indicator.classList.add("indicator");
-        indicatorsGrp.appendChild(indicator);
-    });
-
-    indicatorsGrp.querySelector(".indicator").classList.add('activeIndicator');
-});
-
-document.getElementById("watchVideo").addEventListener("click", () => {
-    let carousel = document.getElementById("carousel1");
-    carousel.style.display = "flex";
-});
+function AutoSlide() {
+    setInterval(() => {
+        start(currentIndex + 1);
+    }, slideInterval);
+}
+start(currentIndex);
+AutoSlide();

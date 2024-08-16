15889626f5298b04b936bf2b4deecc78026b3453
Nassimakhandari <nassomanassima37@gmail.com>
2024-08-16 16:47:07 +0100

    add carousel watch video

--- public/js/app.js
+++ public/js/app.js
@@ -25,7 +25,97 @@ loginBtn.addEventListener("click", () => {
 
 // ! watch video
 
+// let nextBtns = document.querySelectorAll(".next")
+// let previousBtns = document.querySelectorAll(".previous")
+// let containers = document.querySelectorAll(".carousel-container-watch")
+// let openbtn = document.getElementById("open")
+// let carousel8 = document.getElementById("carousel8")
+// openbtn.addEventListener("click", () => {
+//   carousel8.style.display="block"
+// })
 
+// //* sliding function
+// let currentIndex3 = 0
+// const slideImage = (index, myBtn) => {
+
+//     let carouselBtnAttribute = myBtn.getAttribute("carouselBtn")
+
+//     containers.forEach(container => {
+//         if (container.id == carouselBtnAttribute) {
+
+//             //! variables  sal7in  ghir  l lelement  li mt7a9a9 fih chart dyalna  
+//             let slides = container.querySelectorAll(".slide")
+//             let camera = container.querySelector(".carousel")
+//             let slideWidth = slides[0].clientWidth
+//             let indicators = container.querySelectorAll('.indicator')
+
+
+
+
+
+//             //* clikit 3la previous  o ana  aslan f awel image  khasni nmchi  l image lakhra
+//             if (index < 0) {
+//                 index = slides.length - 1
+//             } else if (index >= slides.length) {
+//                 //* clikit 3la next o ana  aslan f akhir image khasni nrje3  l image lewla
+//                 index = 0
+//             }
+
+//             indicators.forEach(indicator => {
+//                 indicator.classList.remove('activeIndicator')
+//             });
+//             indicators[index].classList.add("activeIndicator")
+
+
+//             //* hna bach n7arek l camera dyal lcarousel
+//             camera.style.transform = `translateX(-${slideWidth * index}px)`
+//             currentIndex3 = index
+//         }
+//     });
+
+
+// }
+
+// //* btn dyal next
+// nextBtns.forEach(next => {
+//     next.addEventListener("click", (event) => { slideImage(currentIndex3 + 1, event.target) })
+// });
+
+// //* btn dyal previous
+// previousBtns.forEach(previous => {
+//     previous.addEventListener("click", (event) => { slideImage(currentIndex3 - 1, event.target) })
+// });
+
+
+// //* auto slide 
+// containers.forEach(container => {
+//     //* check wach  l element 3ando attribute auto slide   
+//     if (container.getAttribute("autoslide")) {
+//         let nextBtn = container.querySelector(".next")
+
+//         setInterval(() => {
+//             //* method  kanwarek  biha  3la  btn 
+//             nextBtn.click()
+//         }, 3000);
+//     }
+// });
+
+// //* indicators 
+
+// containers.forEach(container => {
+//     //! variables  sal7in  ghir  l lelement  li mt7a9a9 fih chart dyalna  
+//     let slides = container.querySelectorAll(".slide")
+//     let indicatorsGrp = document.createElement("div")
+//     indicatorsGrp.classList.add("indicators-grp")
+//     container.appendChild(indicatorsGrp)
+
+//     slides.forEach(slide => {
+//         let indicator = document.createElement("div")
+//         indicator.classList.add("indicator")
+//         indicatorsGrp.appendChild(indicator)
+//     });
+//     indicatorsGrp.querySelector(".indicator").classList.add('activeIndicator')
+// });
 
 
 // & Menu
